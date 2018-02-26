cfd624e2ac3a99f06539f6856540148e605c7797
Rafael Lujan <rafael.lujan@outlook.com>
2018-02-26 20:51:43 +0100

    EpisodeDetail Notifications

--- Westeros/Controllers/EpisodeDetailViewController.swift
+++ Westeros/Controllers/EpisodeDetailViewController.swift
@@ -16,13 +16,15 @@ class EpisodeDetailViewController: UIViewController {
     @IBOutlet weak var dateLabel: UILabel!
     
     // MARK: - Properties
-    let model: Episode
+    var model: Episode
     
     init(model: Episode) {
         self.model = model
         super.init(nibName: nil, bundle: nil)
         title = "Episode"
     }
+    
+
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -31,6 +33,16 @@ class EpisodeDetailViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         syncModelWithView()
+        //Alta notificaciones
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(seasonDidChange), name: Notification.Name(SEASON_DID_CHANGE_NOTIFICATION_NAME), object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        //Baja notificaciones
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.removeObserver(self)
     }
     
 
@@ -38,5 +50,21 @@ class EpisodeDetailViewController: UIViewController {
         titleText.text = model.title
         dateLabel.text = "Release Date: \(model.issueDate)"
         summaryLabel.text = model.summary
+    }
+    
+    // MARK: - Notifications
+    @objc func seasonDidChange(notification: Notification) {
+        // Extraer userInfo de la notificación
+        guard let info = notification.userInfo else {
+            return
+        }
+        // Sacar la casa del user info
+        let season = info[SEASON_KEY] as? Season //as es un casting de java
+        //Actualizar el modelo
+        guard let model = season?.sortedEpisodes else { return }//En caso no sea nil model vale eso si no return
+        self.model = model.first!
+        //Sincronizar la vista
+        //self.navigationController?.popViewController(animated: true)
+        syncModelWithView()
     }
 }
